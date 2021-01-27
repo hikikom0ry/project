813cc9104b31b3d7445ec34e6df077022861579d
Zaur <ironetz@rambler.ru>
2021-01-27 12:56:14 +0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+"use strict";
+//alert(20);
+// const result = confirm('Are you here');
+// console.log(result);
+// const answer = prompt("Вам есть 19", "18");
+// console.log(answer);
+// const answers = [];
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+const category = 'toys';
+console.log(`https://someurl.c om/${category}/5`);
+
+ 
